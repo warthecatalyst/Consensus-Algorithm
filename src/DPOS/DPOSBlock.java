f0bebc576c7105b785ecf0f3da2b78cf169f1e0d
ClimberHJX <55943769+ClimberHJX@users.noreply.github.com>
2021-11-13 18:15:23 +0800

    DPOS Uncompleted

--- src/DPOS/DPOSBlock.java
+++ src/DPOS/DPOSBlock.java
@@ -1,13 +1,30 @@
 package DPOS;
 
 import OriginBlock.OriginBlock;
-
 import java.util.Date;
 
-public class DPOSBlock extends OriginBlock {
-    public DPOSBlock(int Index, Date timeStamp, String Data, String Prehash, String Hash) {
-        super(Index, timeStamp, Data, Prehash, Hash);
+class Node{
+    String AddressName;
+    int voteNumber;
+
+    Node(){
+
     }
+
+    Node(String Addr, int number){
+        AddressName = Addr;
+        voteNumber = number;
+    }
+}
+
+public class DPOSBlock extends OriginBlock {
+    public Node blockNode;
+
+    public DPOSBlock(int Index, Date timeStamp, String Data, String Prehash, String Hash, Node blockNode) {
+        super(Index, timeStamp, Data, Prehash, Hash);
+        this.blockNode = blockNode;
+    }
+
 
     @Override
     public boolean Verify() {
